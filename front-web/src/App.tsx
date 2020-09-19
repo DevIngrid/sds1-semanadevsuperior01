add4505df205c498af8b2226875855710cac2d09
Ingrid <diodaaldeia@gmail.com>
2020-09-19 18:44:00 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
+import { type } from 'os';
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hello semana devSuperior!</h1>
-  );
-}
+    <div>
+      <Header/>
+    </div>
+    );
+  }
 
 export default App;
